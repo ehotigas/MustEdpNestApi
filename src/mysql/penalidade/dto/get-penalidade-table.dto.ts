2f23039d2445dd580ec5c776442200ed5f36f6a3
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2024-12-20 08:15:26 -0300

    Update get-penalidade-table.dto.ts

--- src/mysql/penalidade/dto/get-penalidade-table.dto.ts
+++ src/mysql/penalidade/dto/get-penalidade-table.dto.ts
@@ -1,5 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Penalidade } from "../penalidade.entity";
 
+
 export class GetPenalidadeTableDto {
+    @ApiProperty({ type: [Penalidade] })
     data: Penalidade[];
 }
