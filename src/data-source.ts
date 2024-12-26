7ffddcf32c362f7dc371d74ed7c71ab5cd9dd519
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2024-12-26 15:09:49 -0300

    fix: data source connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,10 +3,10 @@ import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
+    host: '10.161.248.71',
+    port: 3307,
+    username: 'eh_o_tigas',
+    password: '#Edp.202412',
     database: 'edp',
     entities: [__dirname + '/../**/*.entity.ts'],
     migrations: [__dirname + '/../**/*_migration.ts'],
